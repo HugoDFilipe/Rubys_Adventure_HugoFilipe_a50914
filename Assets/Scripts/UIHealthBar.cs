ea03569248bc4cd798de8aa51aa4a2d1bf09c0b5
HugoDFilipe <122444521+HugoDFilipe@users.noreply.github.com>
2023-03-28 15:18:32 +0100

    14.Check Your Scripts

--- Assets/Scripts/UIHealthBar.cs
+++ Assets/Scripts/UIHealthBar.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-//public class UIHealthBar : MonoBehaviour
-//// public Image mask;
-   // float originalSize;
+public class UIHealthBar : MonoBehaviour
+{
+    public static UIHealthBar instance { get; private set; }
 
-   // void Start()
-  //  {
-       // originalSize = mask.rectTransform.rect.width;
-    //}
+    public Image mask;
+    float originalSize;
 
-    //public void SetValue(float value)
-   // {
-      //  mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
-    //}
-//}
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.width;
+    }
+
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+}
